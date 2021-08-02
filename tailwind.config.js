77518733c841cbaed3a37158fe26c91baa1d4e50
Duy Nguyen <wasahara11111@gmail.com>
2021-08-02 23:08:23 +0700

    set up tailwind-sass

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        screens: {
+            'sm': {'max': '639px'},
+
+            'md': {'max': '767px'},
+
+            'lg': {'max': '1023px'},
+
+            'xl': {'max': '1279px'},
+        },
+        fontFamily: {
+            'sans': ['Ubuntu', 'Sans-serif']
+        },
+        extend: {
+            spacing: {
+                '72': '18rem',
+                '84': '21rem',
+                '96': '24rem',
+            },
+        },
+    },
+    variants: {},
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('tailwindcss-animatecss')({
+            settings: {
+                animatedSpeed: 1000,
+                heartBeatSpeed: 1000,
+                hingeSpeed: 2000,
+                bounceInSpeed: 750,
+                bounceOutSpeed: 750,
+                animationDelaySpeed: 1000
+            },
+            variants: ['responsive'],
+        }),
+    ],
+}
